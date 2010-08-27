b65b0b86b3af2bdd67886d8cffed656a896ad187
Joerg Bellmann <joerg.bellmann@googlemail.com>
2010-08-27 10:45:44 +0000

    set default values

--- src/main/java/com/googlecode/t7mp/StopServerMojo.java
+++ src/main/java/com/googlecode/t7mp/StopServerMojo.java
@@ -24,20 +24,27 @@ public class StopServerMojo extends AbstractMojo {
 	 * @parameter expression="${t7.tomcat.shutdownCommand}" default-value="SHUTDOWN"
 	 * 
 	 */
-	private String shutdownCommand;
+	private String shutdownCommand = "SHUTDOWN";
 	
 	/**
 	 * 
 	 * @parameter expression="${t7.tomcat.shutdownPort}" default-value="8005"
 	 * 
 	 */
-	private int shutdownPort;
+	private int shutdownPort = 8005;
+	
+	/**
+	 * 
+	 * @parameter expression="${t7.tomcat.shutdownHost}" default-value="localhost"
+	 * 
+	 */
+	private String shutdownHost = "localhost";
 
 	@Override
 	public void execute() throws MojoExecutionException, MojoFailureException {
-		getLog().info("StopMojo -- shutdown Tomcat.");
 		try {
-			Socket socket = new Socket("localhost", shutdownPort);
+			getLog().info("StopMojo -- shutdown Tomcat.");
+			Socket socket = new Socket(shutdownHost, shutdownPort);
 			OutputStream out = socket.getOutputStream();
 			for(int i = 0; i < shutdownCommand.length(); i++){
 				out.write(shutdownCommand.charAt(i));
@@ -51,4 +58,29 @@ public class StopServerMojo extends AbstractMojo {
 			throw new MojoExecutionException(e.getMessage(), e);
 		}
 	}
+
+	public String getShutdownCommand() {
+		return shutdownCommand;
+	}
+
+	public void setShutdownCommand(String shutdownCommand) {
+		this.shutdownCommand = shutdownCommand;
+	}
+
+	public int getShutdownPort() {
+		return shutdownPort;
+	}
+
+	public void setShutdownPort(int shutdownPort) {
+		this.shutdownPort = shutdownPort;
+	}
+
+	public String getShutdownHost() {
+		return shutdownHost;
+	}
+
+	public void setShutdownHost(String shutdownHost) {
+		this.shutdownHost = shutdownHost;
+	}
+	
 }
