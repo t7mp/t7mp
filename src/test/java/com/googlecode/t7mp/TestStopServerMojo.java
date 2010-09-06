b5b1b5ae03f1b61d39b9fe50484da7b73c750334
Joerg Bellmann <joerg.bellmann@googlemail.com>
2010-09-06 09:54:50 +0000

    set runner to 3.8-Runner

--- src/test/java/com/googlecode/t7mp/TestStopServerMojo.java
+++ src/test/java/com/googlecode/t7mp/TestStopServerMojo.java
@@ -1,9 +1,15 @@
 package com.googlecode.t7mp;
 
 import org.apache.maven.plugin.testing.AbstractMojoTestCase;
+import org.junit.Assert;
+import org.junit.internal.runners.JUnit38ClassRunner;
+import org.junit.runner.RunWith;
 
+@RunWith(JUnit38ClassRunner.class)
 public class TestStopServerMojo extends AbstractMojoTestCase {
 	
-	
+	public void testHelloWorld(){
+		Assert.assertTrue(true);
+	}
 
 }
