46ba617c8ffa4e87511a12a8d34cd38ff8f37870
jbellmann <joerg.bellmann@googlemail.com>
2011-08-25 10:20:23 +0200

    #28: make sure not to iterate over file array when listFiles returns null because the directory does not exist or an i/o exception occurred

--- src/main/java/com/googlecode/t7mp/steps/resources/CopySetenvScriptStep.java
+++ src/main/java/com/googlecode/t7mp/steps/resources/CopySetenvScriptStep.java
@@ -13,31 +13,34 @@ import com.googlecode.t7mp.steps.Step;
 import com.googlecode.t7mp.util.TomcatUtil;
 
 public class CopySetenvScriptStep implements Step {
-	
-	private final String PREFIX = "setenv";
 
-	@Override
-	public void execute(Context context) {
-		AbstractT7Mojo mojo = context.getMojo();
-		File tomcatConfigDirectory = mojo.getTomcatConfigDirectory();
-		if(tomcatConfigDirectory == null || !tomcatConfigDirectory.exists()){
-			return;
-		}
-		File tomcatDirectory = tomcatConfigDirectory.getParentFile();
-		File tomcatBinDirectory = new File(tomcatDirectory, "/bin/");
-		File[] setEnvFiles = tomcatBinDirectory.listFiles(new FileFilter(){
-			@Override
-			public boolean accept(File file) {
-				return (file.isFile() && file.getName().startsWith(PREFIX));
-			}
-		});
-		for(File scriptFile : setEnvFiles){
-			try {
-				FileUtils.copyFile(scriptFile, new File(TomcatUtil.getBinDirectory(mojo.getCatalinaBase()), scriptFile.getName()));
-			} catch (IOException e) {
-				throw new TomcatSetupException(e.getMessage(), e);
-			}
-		}
-	}
+    private final String PREFIX = "setenv";
 
+    @Override
+    public void execute(Context context) {
+        AbstractT7Mojo mojo = context.getMojo();
+        File tomcatConfigDirectory = mojo.getTomcatConfigDirectory();
+        if (tomcatConfigDirectory == null || !tomcatConfigDirectory.exists()) {
+            return;
+        }
+        File tomcatDirectory = tomcatConfigDirectory.getParentFile();
+        File tomcatBinDirectory = new File(tomcatDirectory, "/bin/");
+        File[] setEnvFiles = tomcatBinDirectory.listFiles(new FileFilter() {
+            @Override
+            public boolean accept(File file) {
+                return (file.isFile() && file.getName().startsWith(PREFIX));
+            }
+        });
+        // listFiles returns null if binDirectory does not exist
+        // https://github.com/t7mp/t7mp/issues/28
+        if (setEnvFiles != null) {
+            for (File scriptFile : setEnvFiles) {
+                try {
+                    FileUtils.copyFile(scriptFile, new File(TomcatUtil.getBinDirectory(mojo.getCatalinaBase()), scriptFile.getName()));
+                } catch (IOException e) {
+                    throw new TomcatSetupException(e.getMessage(), e);
+                }
+            }
+        }
+    }
 }
