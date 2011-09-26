5d5b4b7b18786f14ccfa5c151f157c2c483c2591
Joerg Bellmann <joerg.bellmann@googlemail.com>
2011-09-27 00:18:09 +0200

    #38: Tests added

--- src/test/java/com/googlecode/t7mp/steps/deployment/ForkedSetupSequenceTest.java
+++ src/test/java/com/googlecode/t7mp/steps/deployment/ForkedSetupSequenceTest.java
@@ -1,17 +1,22 @@
 package com.googlecode.t7mp.steps.deployment;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.maven.artifact.repository.ArtifactRepository;
 import org.junit.Assert;
 import org.junit.Test;
+import org.mockito.Mockito;
 
+import com.google.common.collect.Lists;
+import com.googlecode.t7mp.AbstractT7Mojo;
+import com.googlecode.t7mp.steps.Context;
+import com.googlecode.t7mp.steps.DefaultContext;
 import com.googlecode.t7mp.steps.Step;
-import com.googlecode.t7mp.steps.deployment.TomcatSetupSequenceTest.TestTomcatSetupSequence;
 import com.googlecode.t7mp.steps.resources.ConfigFilesSequence;
 import com.googlecode.t7mp.steps.resources.CopyConfigResourcesFromClasspathSequence;
 import com.googlecode.t7mp.steps.resources.CopyProjectWebappStep;
 import com.googlecode.t7mp.steps.resources.OverwriteWebXmlStep;
-import com.googlecode.t7mp.steps.resources.SetSystemPropertiesStep;
 
 public class ForkedSetupSequenceTest {
 
@@ -37,12 +42,27 @@ public class ForkedSetupSequenceTest {
 //        Assert.assertTrue(eight instanceof SetSystemPropertiesStep);
         Step nine = sequence.getSteps().get(6);
         Assert.assertTrue(nine instanceof OverwriteWebXmlStep);
+        
+        AbstractT7Mojo mojo = Mockito.mock(AbstractT7Mojo.class);
+        List<ArtifactRepository> repositoryList = Lists.newArrayList();
+        Mockito.when(mojo.getRemoteRepos()).thenReturn(repositoryList);
+        Mockito.when(mojo.isAddGithubRepository()).thenReturn(true);
+        
+        sequence.execute(new DefaultContext(mojo));
+        
+        Assert.assertEquals("The github.repo should be in the list", 1, repositoryList.size());
     }
     
     static class TestForkedSetupSequence extends ForkedSetupSequence {
 
         public List<Step> getSteps(){
             return super.sequence;
+        }
+        
+        @Override
+        public void execute(Context context){
+            sequence = new ArrayList<Step>();
+            super.execute(context);
         }
     }
 
